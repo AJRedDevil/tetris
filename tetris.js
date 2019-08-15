8989514787f058a7bed02ed079465feac22193a1
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-08-15 10:14:57 +0545

    feat(tetris): Add offset support to drawMatrix()

--- tetris.js
+++ tetris.js
@@ -8,16 +8,16 @@ context.fillRect(0, 0, canvas.width, canvas.height);
 
 const matrix = [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
 
-function drawMatrix(matrix) {
+function drawMatrix(matrix, offset) {
   matrix.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
         console.log(x, y);
         context.fillStyle = 'red';
-        context.fillRect(x, y, 1, 1);
+        context.fillRect(x + offset.x, y + offset.y, 1, 1);
       }
     });
   });
 }
 
-drawMatrix(matrix);
+drawMatrix(matrix, {x: 5, y: 5});
