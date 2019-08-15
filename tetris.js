37e47ef44bc1d137d523037cbe64e4b318170d10
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-08-15 10:27:12 +0545

    feat(tetris): Clear in beginning of draw()

--- tetris.js
+++ tetris.js
@@ -3,12 +3,12 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
-
 const matrix = [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
 
 function draw() {
+  context.fillStyle = '#000';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
   drawMatrix(player.matrix, player.pos);
 }
 
