3d9bca06a834eaba9e5d46bfb2d0bc5aaf3c20c5
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-08-15 10:37:10 +0545

    feat(tetris): Add keyboard control for left + right

--- tetris.js
+++ tetris.js
@@ -45,4 +45,12 @@ const player = {
   matrix: matrix,
 };
 
+document.addEventListener('keydown', event => {
+  if (event.keyCode === 37) {
+    player.pos.x--;
+  } else if (event.keyCode === 39) {
+    player.pos.x++;
+  }
+});
+
 update();
