6494a025a21f0df555a79065c312ee59dcb3b6ee
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-08-15 10:13:51 +0545

    feat(tetris): Wrap matrix drawing in function

--- tetris.js
+++ tetris.js
@@ -8,12 +8,16 @@ context.fillRect(0, 0, canvas.width, canvas.height);
 
 const matrix = [[0, 0, 0], [1, 1, 1], [0, 1, 0]];
 
-matrix.forEach((row, y) => {
-  row.forEach((value, x) => {
-    if (value !== 0) {
-      console.log(x, y);
-      context.fillStyle = 'red';
-      context.fillRect(x, y, 1, 1);
-    }
+function drawMatrix(matrix) {
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        console.log(x, y);
+        context.fillStyle = 'red';
+        context.fillRect(x, y, 1, 1);
+      }
+    });
   });
-});
+}
+
+drawMatrix(matrix);
